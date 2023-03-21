a9a54e58c5a4c5950d2866c3fb14c4514fe30c89
simon-berner <sberner9@gmail.com>
2023-03-21 11:40:30 +0100

    basic inventory

--- src/app/game/game-level/game-level.component.ts
+++ src/app/game/game-level/game-level.component.ts
@@ -2,6 +2,8 @@ import { Component, HostListener, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
 import { AngularRpg } from '../classes/AngularRpg';
 import { GameElement } from '../interfaces/GameElement';
 import { Inputs } from '../enums/Inputs';
@@ -9,6 +11,8 @@ import { CombatService } from '../combat.service';
 import { AngularRpgService } from '../angular-rpg.service';
 import { Player } from '../classes/Player';
 import { Enemy } from '../classes/Enemy';
+import { Item } from '../classes/Item';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-game-level',
@@ -22,7 +26,8 @@ export class GameLevelComponent implements OnInit {
   constructor(
     private router: Router,
     private combatService: CombatService,
-    private angularRpgService: AngularRpgService
+    private angularRpgService: AngularRpgService,
+    public itemInteractionDialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
@@ -57,10 +62,18 @@ export class GameLevelComponent implements OnInit {
     }
 
     if(this.angularRpg.opponent) {
-      console.log('opponent found');
       this.goToCombat(this.angularRpg.player, this.angularRpg.opponent);
       this.angularRpg.opponent = undefined;
     }
+
+    // TODO: I need to check for a item interatcion here
+    // TODO: and show a dialog box with the item's description and options
+    if(this.angularRpg.item) {
+      this.showItemInteractionDialog(this.angularRpg.item);
+      this.angularRpg.item = undefined;
+    }
+
+
 
     // this forces the view to update
     let newElementsArray: GameElement[] = [];
@@ -74,5 +87,33 @@ export class GameLevelComponent implements OnInit {
     this.combatService.setPlayer(player);
     this.combatService.setEnemy(opponent);
     this.router.navigate(['/game/combat']);
+  }
+
+  showItemInteractionDialog(item: Item): void {
+    const config: MatDialogConfig = {
+      disableClose: true,
+      data: {
+        title: `You found a ${item.name}!`,
+        message: item.description,
+        btn1: {
+          title: 'Use',
+          matStyle: 'primary',
+          action: () => {
+            item.action(this.angularRpg.player, item.id);
+            this.itemInteractionDialog.closeAll();
+          },
+        },
+        btn2: {
+          title: 'Pick Up',
+          matStyle: 'primary',
+          action: () => {
+            this.angularRpg.player.inventory.push(item);
+            this.itemInteractionDialog.closeAll();
+          },
+        }
+      }
+    };
+
+    this.itemInteractionDialog.open(DialogComponent, config);
   }
 }
